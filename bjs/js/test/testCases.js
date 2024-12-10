c98e5b1528c662ab7d122717b463b790e57c77ee
mo-hi <mohi@outlook.de>
2024-12-10 14:19:05 +0100

    renaming

--- bjs/js/test/testCases.js
+++ bjs/js/test/testCases.js
@@ -5,9 +5,9 @@ function testCases(myTest) {
 function testcase_Proto_Depth(myTest) {
     let fname = arguments.callee.name;
     
-    myTest.Equal([1,2,3].depth(),1,fname)
-    myTest.Equal([1,[2],3].depth(),2,fname)
-    myTest.Equal([1,[2, [4]],3].depth(),3,fname)
+    myTest.Equal([1, 2, 3].depth(),1,fname)
+    myTest.Equal([1, [2], 3].depth(),2,fname)
+    myTest.Equal([1, [2, [3]]].depth(),3,fname)
     myTest.Equal([1,[2, [[1,[2, [4]],3]]],3].depth(),6,fname)
     myTest.Equal([1,[2, [[1,[2, [4]],3]]],3].depth(6),6,fname)
     myTest.Equal([1,[2, [[1,[2, [4]],3]]],3].depth(5),5,fname)
