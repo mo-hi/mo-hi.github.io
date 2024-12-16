8cd5f71ef24851c721dd6fd0531d5a44f9b23efe
mo-hi <mohi@outlook.de>
2024-12-16 17:16:47 +0100

    Update

---
+++ Mathfit/js/nav.js
@@ -0,0 +1,6 @@
+function toggleDown(a) {
+    let down = a.parentElement.querySelector("down")
+    down.style.display = wenn(down.style.display == "block", "none", "block")}
+
+function wenn(condition, trueValue, falseValue) {
+    return condition ? trueValue : falseValue;}
