281ff10424a631dba5ef1eb4e23b6cf505941565
mo-hi <mohi@outlook.de>
2024-12-22 15:30:12 +0100

    Added dynamic sidebar

--- navbar/js/nav.js
+++ navbar/js/nav.js
@@ -1,3 +1,4 @@
+// MOHI: Change this to class toggling
 function toggleDown(a) {
     let down = a.parentElement.querySelector('down')
     down.style.display = wenn(down.style.display == 'block', 'none', 'block')
@@ -5,4 +6,32 @@ function toggleDown(a) {
 
 function wenn(condition, trueValue, falseValue) {
     return condition ? trueValue : falseValue;
+}
+
+
+// dynamic sidebar generation
+function nav_AutoFillSidebar(targetID, data) {
+    let ul = document.getElementById(targetID)
+    if (!ul.classList.contains("sidebar-auto-fill")) return
+
+    ul = _ulist(ul, data)
+}
+
+function _ulist(parent, items) {
+    for (let item of items) {
+        let li = document.createElement('li');
+        li.innerHTML = item.name;
+        if (Object.keys(item).includes('children')) {
+            li.setAttribute('onclick', 'toggleDown(this)');
+            let drop = document.createElement('drop'); // parent element of ego li
+            let down = document.createElement('down'); // next neighbour element of ego li
+            down = _ulist(down,item["children"])
+            drop.appendChild(li)
+            drop.appendChild(down)
+            parent.appendChild(drop)
+        } else {
+            parent.appendChild(li)
+        }
+    }
+    return parent
 }
---
+++ navbar/js/sideData.js
@@ -0,0 +1,35 @@
+  
+// Example sidebar data
+const sidebarData = [
+    { name: "Australia" },
+    {
+        name: "Europe",
+        children: [
+            {
+            name: "Germany",
+            children: [
+                {
+                name: "Bavaria",
+                children: [
+                    {
+                    name: "Munich",
+                    children: [
+                        { name: "Schwabing" },
+                        { name: "Neuhausen" },
+                        { name: "Feldmoching" },
+                    ],
+                    },
+                    { name: "Regensburg" },
+                    { name: "Rosenheim" },
+                ],
+                },
+                { name: "Saxony" },
+                { name: "NRW" },
+            ],
+            },
+            { name: "France" },
+            { name: "Italy" },
+        ],
+        },
+        { name: "Zoomania" },
+  ];
