6a067bfa7390820023194224099856b7028634d4
mo-hi <mohi435673@outlook.de>
2025-01-22 21:17:06 +0100

    Update

--- editable HTML/js/editableHTML.js
+++ editable HTML/js/editableHTML.js
@@ -1,132 +1,211 @@
+// region Init
 
-function b_editableHTML_init() {
-    document.querySelectorAll('.js-edit-btn').forEach(editWrapper => {
-        let editButton = editWrapper.firstElementChild
-        let saveButton = editButton.nextElementSibling
-        let editTarget = document.getElementById(editWrapper.dataset.targetId)
-        let editableDiv = editTarget.firstElementChild
-        let discardButton = editableDiv.nextElementSibling
-        
-        editButton = addEventListener_ClickTouch(editButton, ToggleEditSaveDiscard)
-        saveButton = addEventListener_ClickTouch(saveButton, ToggleEditSaveDiscard)
-        discardButton = addEventListener_ClickTouch(discardButton, ToggleEditSaveDiscard)
+function b_editableHTML_init(elementId = "body") {
+    let container = document.getElementById(elementId);
+    if (elementId == "body") container = document.body
+    
+    container.DescendantsWithClass('.js-edit-btn').forEach(editBtn => {   
+        let divInitializer = new cls_editableHTML_divInitializer(editBtn)
+        let node = divInitializer.node()
+        let editButton = divInitializer.editButton()
+        let saveButton = divInitializer.saveButton()
+        let discardButton = divInitializer.discardButton()
+       
 
-        editButton = addEventListener_ClickTouch(editButton, b_edit_edit)
-        saveButton = addEventListener_ClickTouch(saveButton, b_edit_save)
-        discardButton = addEventListener_ClickTouch(discardButton, b_edit_discard)
+        // button events
+        editButton.JSEvent_AddClickTouch(ToggleEditSaveDiscard)
+        saveButton.JSEvent_AddClickTouch(ToggleEditSaveDiscard)
+        discardButton.JSEvent_AddClickTouch(ToggleEditSaveDiscard)
+
+        editButton.JSEvent_AddClickTouch(b_edit_edit)
+        saveButton.JSEvent_AddClickTouch(b_edit_unedit)
+        discardButton.JSEvent_AddClickTouch(b_edit_unedit)
+
+        // key events
+        document.addEventListener('keydown', (event) => {
+            if (event.key === 'Escape') b_edit_ESC()
+        });   
 });
 }
 
-// Pure Desktop Application
-function addEventListener_ClickTouch(element, functionName) {
-    if (element.classList.contains("js-event")) {
-        element.addEventListener('click', functionName)
-        element.addEventListener('touchstart', function(event) {
-            event.preventDefault(); // Prevent mouse events
-            functionName(event);     // Call your function
-            });
+
+class cls_editableHTML_divInitializer {
+    constructor(editBtnDiv) {
+        this.editBtnDiv = editBtnDiv
     }
 
-    return element
-}
-
-function addEventListener_ClickTouch(element, functionName) {
-    if (element.classList.contains("js-event")) {
-        element.addEventListener('click', functionName)
-        element.addEventListener('touchstart', function(event) {
-            event.preventDefault(); // Prevent mouse events
-            functionName(event);     // Call your function
-            });
+    node() {
+        // if btn wrapper is inside the node, set the link (overwrite)
+        let IsNode = this.editBtnDiv.closest('.js-edit-node')
+        if (IsNode) this.editBtnDiv.dataset.targetId = IsNode.id
+    
+        let node = document.getElementById(this.editBtnDiv.dataset.targetId)
+        
+        this.validate(node)
+        return node
     }
 
-    return element
+    editButton() {
+        let IsEditButton = this.editBtnDiv.DescendantsWithClass('.js-edit-edit')
+        if (IsEditButton.length == 1) return IsEditButton[0]
+        
+        assert(IsEditButton.length == 0)
+        let editButton = this.editBtnDiv.firstElementChild; 
+        
+        this.validate(editButton)
+        editButton.classList.addX('js-edit-edit');
+        return editButton
+    }
+
+    saveButton() {
+        let IsSaveButton = this.editBtnDiv.DescendantsWithClass('.js-edit-save')
+        if (IsSaveButton.length == 1) return IsSaveButton[0]
+        
+        assert(IsSaveButton.length == 0)
+        let saveButton = this.editButton().nextElementSibling
+
+        this.validate(saveButton)
+        saveButton.classList.addX('js-edit-save');
+        return saveButton
+    }
+
+    discardButton() {   
+        let IsDiscardButton = this.editBtnDiv.DescendantsWithClass('.js-edit-discard') 
+        if (IsDiscardButton.length == 1) return IsDiscardButton[0] 
+
+        assert(IsDiscardButton.length == 0)
+        let node= this.node()
+        let DiscardButtons= node.DescendantsWithClass('.js-edit-discard')
+        
+        this.validate(DiscardButtons)
+        return DiscardButtons[0] 
+    }
+
+    validate(div) {
+        assert(div != undefined)
+    }
 }
+
+class cls_editableHTML_divTracer {
+    constructor(anyButton) {
+        this.btn = null
+        this.node = null
+        this.editButton = null
+        this.saveButton = null
+        this.discardButton = null
+        this.editableDivs = null
+        this.trace(anyButton)
+    }
+
+    trace(anyButton) {
+        if (anyButton.classList.contains('js-edit-discard')) {
+            this.discardButton = anyButton}
+        
+        if (anyButton.closest('.js-edit-node')) {
+            this.node = anyButton.closest('.js-edit-node')}
+        
+        if (this.node) {
+            this.btn = document.querySelectorAll('.js-edit-btn[data-target-id="' + this.node.id + '"]')[0]}
+        else {
+            this.btn = anyButton.closest('.js-edit-btn')
+            this.node = document.getElementById(this.btn.dataset.targetId)}
+
+        this.editButton = this.btn.DescendantsWithClass('.js-edit-edit')[0]
+        this.saveButton = this.btn.DescendantsWithClass('.js-edit-save')[0]
+        this.discardButton = this.btn.DescendantsWithClass('.js-edit-discard')[0] || this.node.DescendantsWithClass('.js-edit-discard')[0]
+
+        this.editableDivs = this.node.DescendantsWithClass('.js-edit-div')
+    }
+
+    Buttons() {
+        return [this.editButton, this.saveButton, this.discardButton]
+    }
+}
+
 
 function ToggleEditSaveDiscard(event) {
-    let egoElement = divFromEvent(event)
-    let [editButton, saveButton, discardButton, editableDiv, textareaDiv] = _return_edit_save_discard_editable_textarea(egoElement)
+    let egoElement = DOM_ElementFromJSEvent(event)
+    let Tracer = new cls_editableHTML_divTracer(egoElement)
 
-    if (!editButton) return 
-    editButton.classList.toggle('hidden');
-    saveButton.classList.toggle('hidden');
-    discardButton.classList.toggle('hidden');
+    for (let button of Tracer.Buttons()) {
+        button.classList.toggle('hidden');
+    }
+    Tracer.discardButton.dataset.status = Tracer.discardButton.classList.contains('hidden') ? '' : 'active';
 }
+
+// region Button Clicks
 
 function b_edit_edit(event) {
-    divElement = divFromEvent(event)
+    let divElement = DOM_ElementFromJSEvent(event)
+    let editableDivs = new cls_editableHTML_divTracer(divElement).editableDivs
+    let minHeight = 40
 
-    let editableTarget = document.getElementById(divElement.parentElement.dataset.targetId)
-    let editableDiv = editableTarget.firstElementChild
-    
-    // padding: store to dataset and modify element styling (to overwrite css styling)
-    editableDiv.dataset.stylePadding = getComputedStyle(editableDiv).padding
-    editableDiv.style.padding = "0px"
-    // width: store to dataset and modify   -> if padding = 0 would not be applied, then then padding must also be substracted
-    editableDiv.dataset.originalWidth = Math.round(editableDiv.offsetWidth  -2*parseFloat(getComputedStyle(editableDiv).borderWidth)) +"px"
-    let wwidth = (parseInt(editableDiv.offsetWidth, 10)-36) + "px"
-    editableDiv.style.setProperty("width", wwidth, "important")
-    // innerHTML: store to dataset (used when discard is clicked later)
-    editableDiv.dataset.originalInnerHTML = editableDiv.innerHTML
-    // value for textarea
-    let fDict = CONST_EDITABLE_HTML_FUNCTION_CALLS
-    if (fDict["edit"]) {
-        editableDiv.dataset.originalTextAreaValue = fDict["edit"](editableDiv.innerHTML)
-    } else {
-        editableDiv.dataset.originalTextAreaValue = editableDiv.innerHTML}
+    for (let editableDiv of editableDivs) {
+        editableDiv = b_edit_edit_SetDataset(editableDiv)
 
-    // delete inner HTML and repalce by textarea
-    editableDiv.innerHTML = ''
-    editableDiv.appendChild(__b_textarea(editableDiv))  
-}
+        if (editableDiv.dataset.originalStylePadding) {
+            let padding = 0 + "px"
+            editableDiv.style.setProperty("padding", padding)
+            editableDiv.dataset.padding = padding}
 
-function b_edit_save(event) {
-    divElement = divFromEvent(event)
-    let [editButton, saveButton, discardButton, editableDiv, textareaDiv] = _return_edit_save_discard_editable_textarea(divElement)
-    
-    let fDict = CONST_EDITABLE_HTML_FUNCTION_CALLS
-    if (fDict["save"]) {
-        editableDiv.innerHTML = fDict["save"](textareaDiv.value)
-    } else {
-        editableDiv.innerHTML = textareaDiv.value
+        if (editableDiv.dataset.originalHeight) {
+            let height = Math.max(editableDiv.dataset.originalHeight, minHeight) + "px"
+            editableDiv.style.setProperty("height", height)
+            editableDiv.dataset.height = height}
+        
+        if (editableDiv.dataset.originalInnerHTML) {
+            editableDiv.innerHTML = ''}
+
+        editableDiv.appendChild(__b_textarea(editableDiv))  
     }
-
-    _unedit(divElement)
 }
 
-function b_edit_discard(event) {
-    divElement = divFromEvent(event)
-    let [editButton, saveButton, discardButton, editableDiv, textareaDiv] = _return_edit_save_discard_editable_textarea(divElement)
-
-    editableDiv.innerHTML = editableDiv.dataset.originalInnerHTML
+function b_edit_edit_SetDataset(editableDiv) {
+    let fDict = CONST_EDITABLE_HTML_FUNCTION_CALLS
     
-    _unedit(divElement)
+    editableDiv.dataset.originalStylePadding = getComputedStyle(editableDiv).padding
+    editableDiv.dataset.originalInnerHTML = editableDiv.innerHTML.trim()
+    editableDiv.dataset.originaTextAreaValue = fDict["edit"](editableDiv.innerHTML.trim())
+    editableDiv.dataset.originalHeight = parseInt(getComputedStyle(editableDiv).height) +
+    parseInt(getComputedStyle(editableDiv).paddingTop) + parseInt(getComputedStyle(editableDiv).paddingBottom)
+
+    return editableDiv
 }
 
-function divFromEvent(event) {
-    let divElement = null;
-    if (event instanceof PointerEvent) divElement = event.target;   // if eventlistener was used
-    if (event instanceof HTMLElement) divElement = event;           // if setAttribute / direct HTML was used
-    if (event instanceof TouchEvent) divElement = event.target;     // if eventlistener was used
-    if (divElement == null) return
+function b_edit_unedit(event) {
+    let fDict = CONST_EDITABLE_HTML_FUNCTION_CALLS
+    let divElement = DOM_ElementFromJSEvent(event)
+    let editableDivs = new cls_editableHTML_divTracer(divElement).editableDivs
 
-    /// target may be pointing to childrean of the intended target. Loop Up 5 parents. if not found, abort.
-    for (i = 0; i < 5; i++) {
-        if (!divElement.classList.contains('js-event')) divElement = divElement.parentElement}      
-    if (!divElement.classList.contains('js-event')) return
-
-    return divElement
+    for (let editableDiv of editableDivs) {
+        let innerHTML = ''
+        if (divElement.classList.contains('js-edit-save')) {
+            let textarea = document.getElementById(editableDiv.id + "-textarea")
+            innerHTML = fDict["save"](textarea.value)}
+        if (divElement.classList.contains('js-edit-discard')) {
+            innerHTML = editableDiv.dataset.originalInnerHTML}
+        
+        editableDiv.innerHTML = innerHTML
+        _restoreOriginal(editableDiv)
+    }
 }
+
+function b_edit_ESC() {
+    document.querySelectorAll('.js-edit-discard[data-status="active"]').forEach(divElement => {
+        b_edit_unedit(divElement); 
+        ToggleEditSaveDiscard(divElement);
+    });
+}
+
 
 function __b_textarea(editableDiv) {
-    const textarea = document.createElement("textarea");
+    let textarea = document.createElement("textarea");
     // take over dataset
-    textarea.value = editableDiv.dataset.originalTextAreaValue
-    textarea.style.padding = editableDiv.dataset.stylePadding
-
-    // take over width and height, modified id
-    textarea.style.width = (editableDiv.offsetWidth) + "px"
-    textarea.style.height = (editableDiv.offsetHeight) + "px"
-    textarea.id = editableDiv.id + "-textarea"
+    textarea.id             = editableDiv.id + "-textarea"
+    textarea.value          = editableDiv.dataset.originaTextAreaValue  
+    textarea.style.padding  = editableDiv.dataset.originalStylePadding
+    textarea.style.height   = '100%'
+    textarea.style.width    = "100%"
 
     // textarea syle
     textarea.style.boxSizing = "border-box";       // size of textarea box: border-box = same as div, content-box = based on content
@@ -134,41 +213,65 @@ function __b_textarea(editableDiv) {
     textarea.style.borderRadius = '3px';
     textarea.style.resize = "none";
 
+    //vertical scrollbar if needed
+    textarea.style.overflowY = 'auto'; 
+    textarea.style.overflowX = 'hidden';
+
     return textarea
 }
 
-function _unedit(egoElement) {
-    let [editButton, saveButton, discardButton, editableDiv, textareaDiv] = _return_edit_save_discard_editable_textarea(egoElement)
-
-    // resert element styling (to activate back css styling)
-    editableDiv.style.padding = ''
-    editableDiv.style.width = ''
-
-    if (textareaDiv) textareaDiv.remove()
-
+function _restoreOriginal(editableDiv) {
+    if (editableDiv.dataset.padding) {
+        editableDiv.style.padding = ''
+        editableDiv.dataset.padding = ''}
+    
+    if (editableDiv.dataset.height) {
+        editableDiv.style.height = ''
+        editableDiv.dataset.height = ''}
+    
+    let textarea = document.getElementById(editableDiv.id + "-textarea")
+    if (textarea) textarea.remove()
 }
 
-function _return_edit_save_discard_editable_textarea(egoElement) {
-    if (!egoElement.classList.contains('js-event')) return
-    // fyi: the egoElement is one of the three
-    let editButton = undefined
-    let saveButton = undefined
-    let discardButton = undefined
-    // and the editable div + textarea (in case of save and discard )
-    let editableDiv = undefined
-    let textareaDiv = undefined
 
-    if (egoElement.parentElement.classList.contains("js-edit-btn")) {
-        editButton = egoElement.parentElement.firstElementChild
-        saveButton = editButton.nextElementSibling
-        discardButton = document.getElementById(egoElement.parentElement.dataset.targetId).firstElementChild.nextElementSibling}
-    if (egoElement.parentElement.classList.contains("js-edit-div")) {
-        sources = document.querySelectorAll('div[data-target-id="' + egoElement.parentElement.id + '"]')
-        editButton = sources[0].firstElementChild
-        saveButton = editButton.nextElementSibling
-        discardButton = egoElement
+// region Templates
+
+function EditableDiv_TemplateButtons(targetID, includeDiscard = false) {
+    // Create the main div
+    let mainDiv = document.createElement('div');
+    mainDiv.classList.add('js-edit-btn');
+    mainDiv.dataset.targetId = targetID;
+
+    // Create the edit button div
+    let editButton = document.createElement('div');
+    editButton.classList.add('btn', 'blue', 'js-event');
+    editButton.innerHTML = b_svg("svg-icon-edit-18")
+
+    // Create the save button div
+    let saveButton = document.createElement('div');
+    saveButton.classList.add('btn', 'blue', 'js-event', 'hidden');
+    saveButton.innerHTML = b_svg("svg-icon-save-18")
+
+    // Append the buttons to the main div
+    mainDiv.appendChild(editButton);
+    mainDiv.appendChild(saveButton);
+
+    if (includeDiscard) {
+        // Create the discard button div
+        let discardButton = document.createElement('div');
+        discardButton.classList.add('btn', 'blue', 'js-event', 'hidden', 'js-edit-discard');
+        discardButton.innerHTML = b_svg("svg-icon-discard-12");
+        mainDiv.appendChild(discardButton);
     }
-    editableDiv = discardButton.parentElement.firstElementChild
-    if (egoElement === saveButton  || egoElement === discardButton) textareaDiv = editableDiv.firstElementChild
-    return [editButton, saveButton, discardButton, editableDiv, textareaDiv]
+
+    return mainDiv;
+}
+
+function EditableDiv_TemplateDiscard (id) {
+            // Create the discard button div
+            let discardButton = document.createElement('div');
+            discardButton.classList.add('btn', 'blue', 'js-event', 'hidden', 'js-edit-discard');
+            discardButton.innerHTML = b_svg("svg-icon-discard-12");
+            if (id) discardButton.id = id
+            return discardButton
 }
