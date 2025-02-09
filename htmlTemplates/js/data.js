33ab8647853548e6936346f16fa4f48173ce1dcc
mo-hi <mohi435673@outlook.de>
2025-02-09 17:19:06 +0100

    htmlTables Update

--- htmlTemplates/js/data.js
+++ htmlTemplates/js/data.js
@@ -1,20 +1,8 @@
 const FromFile_data = [
-    {
-        "A": " ",
-        "B": " ascasc",
-        "C": " ",
-        "D": " "
-    },
-    {
-        "A": " ",
-        "B": " ",
-        "C": " ",
-        "D": " "
-    },
-    {
-        "A": " ",
-        "B": " ",
-        "C": " ",
-        "D": " "
-    }
+    {"Country": "Germany", "City": "Munich", "River": "Isar", "Food": "Beer"},
+    {"Country": "France", "City": "Paris", "River": "Seine", "Food": "Baguette"},
+    {"Country": "Italy", "City": "Rome", "River": "Tiber", "Food": "Pasta"},
+    {"Country": "Spain", "City": "Madrid", "River": "Manzanares", "Food": "Paella"},
+    {"Country": "Japan", "City": "Tokyo", "River": "Sumida", "Food": "Sushi"},
+    {"Country": "USA", "City": "New York", "River": "Hudson", "Food": "Burger"}
 ]
