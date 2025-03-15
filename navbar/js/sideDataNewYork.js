7ea858c1f53042f93e166417fc31b4105c533151
mo-hi <mohi435673@outlook.de>
2025-03-15 09:58:53 +0100

    Added data js

---
+++ navbar/js/sideDataNewYork.js
@@ -0,0 +1,34 @@
+  
+// Example sidebar data
+const sidebarDataCopy = [
+    { name: "Europe" },
+    {
+        name: "America",
+        children: [
+            {
+            name: "USA",
+            children: [
+                {
+                name: "New York",
+                children: [
+                    {
+                    name: "New York City",
+                    children: [
+                        { name: "Manhatten" },
+                        { name: "Queens" },
+                        { name: "Brooklyn" },
+                    ],
+                    },
+                    { name: "Watertown" },
+                ],
+                },
+                { name: "California" },
+                { name: "Texas" },
+            ],
+            },
+            { name: "Canada" },
+            { name: "Mexico" },
+        ],
+        },
+        { name: "Asia" },
+  ];
