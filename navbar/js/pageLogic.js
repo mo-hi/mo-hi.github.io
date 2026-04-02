f30fcf7f229b4856730852f6f6ec9488de3de142
mo-hi <mohi435673@outlook.de>
2026-04-02 13:00:51 +0200

    nav 1-2-3

--- navbar/js/pageLogic.js
+++ navbar/js/pageLogic.js
@@ -42,32 +42,39 @@ function navbar_autoclose() {
 function navbar_hover() {
     tag_hover.classList.add("active")
     tag_click.classList.remove("active")
+    tag_autoclose.classList.remove("active")
     Update()
 }
 
-function One() {
-    tag_1.classList.add("active")
-    tag_2.classList.remove("active")
-    Update()
-}
-
-function Two() {
+function NumberNavs(n) {
     tag_1.classList.remove("active")
-    tag_2.classList.add("active")
+    tag_2.classList.remove("active")
+    tag_3.classList.remove("active")
+    tag_gap.classList.remove("active")
+    tag_gap10.classList.remove("active")
+    tag_gap20.classList.remove("active")
+    tag_gap.classList.add("hidden")
+    tag_gap10.classList.add("hidden")
+    tag_gap20.classList.add("hidden")
+    if (n == 1) tag_1.classList.add("active")
+    if (n == 2) tag_2.classList.add("active")
+    if (n == 3) {
+        tag_3.classList.add("active")
+        tag_gap.classList.add("active")
+        tag_gap.classList.remove("hidden")
+        tag_gap10.classList.remove("hidden")
+        tag_gap20.classList.remove("hidden")
+    }
     Update()
 }
 
-function OldTemplate() {
-    tag_new.classList.remove("active")
-    tag_old.classList.add("active")
-
-    Update()
-}
-
-function NewTemplate() {
-    tag_old.classList.remove("active")
-    tag_new.classList.add("active")
-
+function Gap(n) {
+    tag_gap.classList.remove("active")
+    tag_gap10.classList.remove("active")
+    tag_gap20.classList.remove("active")
+    if (n == 0) tag_gap.classList.add("active")
+    if (n == 10) tag_gap10.classList.add("active")
+    if (n == 20) tag_gap20.classList.add("active")
     Update()
 }
 
@@ -77,30 +84,37 @@ function UpdateNavBlock() {
 
     // static of fixed
     if (tag_fixed.classList.contains("active")) {
-        parentNav.classList.add("fixed", "wr-800-400", "top-0")
+        parentNav.classList.add("nav-fixed", "w-responsive-xl")
         let statusbar = document.getElementById('id-status1')
         statusbar.classList.remove("mt-30i")
         statusbar.classList.add("mt-82i")
     }
-
     
-
     // 1 or 2 navs (and new and old templates insice _CreateNavExample)
     if (tag_1.classList.contains("active") ) 
         parentNav.appendChild(_CreateNavExample())
     
+    if (tag_2.classList.contains("active") || tag_3.classList.contains("active"))
+        parentNav.classList.add("nav-multi")
+    
     if (tag_2.classList.contains("active") ) {
         let navLeft = _CreateNavExample()
-        navLeft.classList.add("w-55")
-
         let navRight = _CreateNavExample()
-        navRight.classList.add("w-45")
-
+        navRight.classList.add("nav-right")
         parentNav.appendChild(navLeft)
         parentNav.appendChild(navRight)
-        parentNav.classList.add("flex")
     }
 
+    if (tag_3.classList.contains("active")) {
+        let navLeft = _CreateNavExample()
+        let navCenter = _CreateNavExample()
+        let navRight = _CreateNavExample()
+        navCenter.classList.add("nav-middle")
+        navRight.classList.add("nav-right")
+        parentNav.appendChild(navLeft)
+        parentNav.appendChild(navCenter)
+        parentNav.appendChild(navRight)
+    }
 
     // hover of clickable
     let drops = parentNav.querySelectorAll('drop, div.drop');
@@ -117,6 +131,14 @@ function UpdateNavBlock() {
         drops.forEach(drop => drop.classList.add("click-toggle"))
         drops.forEach(drop => drop.classList.add("auto-close"))
         _nav_initClickListeners()
+    }
+
+    // gap in case of three nav parts
+    if (tag_gap10.classList.contains("active")) {
+        parentNav.classList.add("nav-gap-10")
+    }
+    if (tag_gap20.classList.contains("active")) {
+        parentNav.classList.add("nav-gap-20")
     }
 }
 
@@ -153,31 +175,17 @@ function _CreateNavExample() {
     nav.appendChild(_CreateLink("Link"))
     nav.appendChild(_CreateLink("Call"))
 
-    if (tag_new.classList.contains("active")) {
-        let drop = document.createElement('div')
-        drop.classList.add("drop")
-        drop.appendChild(_CreateLink("DropDown"))
+    let drop = document.createElement('div')
+    drop.classList.add("drop")
+    drop.appendChild(_CreateLink("DropDown"))
 
-        let down = document.createElement('div')
-        down.classList.add("down")
-        down.appendChild(_CreateLink("Link 1"))
-        down.appendChild(_CreateLink("Link 2"))
-        down.appendChild(_CreateLink("Link 3"))
-        drop.appendChild(down)
-        nav.appendChild(drop)
-    }
-
-    if (tag_old.classList.contains("active")) {
-        let drop = document.createElement('drop')
-        drop.appendChild(_CreateLink("DropDown"))
-
-        let down = document.createElement('down')
-        down.appendChild(_CreateLink("Link 1"))
-        down.appendChild(_CreateLink("Link 2"))
-        down.appendChild(_CreateLink("Link 3"))
-        drop.appendChild(down)
-        nav.appendChild(drop)
-    }
+    let down = document.createElement('div')
+    down.classList.add("down")
+    down.appendChild(_CreateLink("Link 1"))
+    down.appendChild(_CreateLink("Link 2"))
+    down.appendChild(_CreateLink("Link 3"))
+    drop.appendChild(down)
+    nav.appendChild(drop)
 
     return nav
 
